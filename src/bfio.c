b637148331951ca7d623af9dd165af22168a3855
Garmine <mikro001@gmail.com>
2014-12-02 08:02:16 +0100

    Added skeleton of BF stdio API.

--- src/bfio.c
+++ src/bfio.c
@@ -1,12 +1,63 @@
 #include "bfio.h"
 
+#include "util.h"
+
 #include <stdio.h>
 
+Syscall calls[256];
+int init=0;
+
+SyscallIn inCall = NULL;
+SyscallOut outCall = NULL;
+
+void initBfio(){
+	int i;
+	for (i=0; i<256; i++) calls[i].code=-1;
+}
+
+int addSyscall(Syscall c){
+	int i = c.code;
+
+	// Check for invalid index
+	if(i<0 || i>255){
+		char buff[16];
+		sprintf(buff, "%d", i);
+		errors("addSyscall()", "Invalid call index: ", buff);
+		return 0;
+	}
+
+	
+	// Check for usage
+	if(calls[i].code != -1){
+		char buff[16];
+		sprintf(buff, "%d", i);
+		errors("addSyscall()", "Index is already being used: ", buff);
+		return 0;
+	}
+
+	// Register syscall
+	call[i]=c;
+	return 1;
+}
+
 char input(){
-	return getchar();
+	if(inCall){
+		// TODO use return value
+		char* c;
+		inCall(c);
+		return *c;
+	}else{
+		return getchar();
+	}
 }
 
 void output(char c){
-	putchar(c);
+	if(outCall){
+		// TODO double-escape
+		// TODO use return value
+		outCall(c);
+	}else{
+		putchar(c);
+	}
 }
 
--- src/bfio.h
+++ src/bfio.h
@@ -1,6 +1,16 @@
 #ifndef BFIO_H
 #define BFIO_H
 
+typedef int(*SyscallOut)(char);
+typedef int(*SyscallIn)(char*);
+typedef struct{
+	int code;
+	SyscallOut out;
+	SyscallIn in;
+}Syscall;
+
+int addSyscall(Syscall c);
+
 char input();
 void output(char c);
 
