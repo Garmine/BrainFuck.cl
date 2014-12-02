67403d2a922385fc0aa66baacd49796a645a8866
Garmine <mikro001@gmail.com>
2014-12-02 21:17:42 +0100

    Added subfolder for BrainFuck codes and examples.

--- src/main.c
+++ src/main.c
@@ -6,33 +6,30 @@
 #include "host.h"
 #include "host-interpreter.h"
 
-// Prints "Schorbino!\n" to stdout
-const char* code = "\
-++++[>++++[>+++++<-]<-]>>+++.    83=S\
-<++++[>++++<-]>.                 99=c\
-+++++.                          104=h\
-+++++++.                        111=o\
-+++.                            114=r\
-<++++[>----<-]>.                 98=b\
->+++++[>++++++<-]>+++.<<         33=!\
-<+++[>++++<-]>.                 110=n\
-+.                              111=o\
->>>++[>+++++<-]>.                10=\n";
-
 int main(int argc, char** argv){
 	if(!initBrainfuckDotCl())
 		fatalError("Failed to initialize Brainfuck.cl");
 
+
+	// Read in some BF code from some file
+	char* code = readFile("bf/schorbino.bf");
+	if(code==NULL)
+		fatalError("Failed to read in BF code");
+
+		
 	// Create host
 	Host* host = initHost(code, 128);
-
+	free(code);
 	if(host==NULL)
 		fatalError("Failed to create host thread");
+
 
 	// Run host
 	run(host);
 
+
 	// KTHXBAI
+	freeHost(host);
 	return 0;
 }
 
