044010934f1a85b947aef2d9ffa6b7f43e87bfd3
Garmine <mikro001@gmail.com>
2014-12-02 13:57:41 +0100

    Added initialiazation code.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "init.h"
+#include "util.h"
 #include "host.h"
 #include "host-interpreter.h"
 
@@ -18,8 +20,19 @@ const char* code = "\
 >>>++[>+++++<-]>.                10=\n";
 
 int main(int argc, char** argv){
+	if(!initBrainfuckDotCl())
+		fatalError("Failed to initialize Brainfuck.cl");
+
+	// Create host
 	Host* host = initHost(code, 128);
+
+	if(host==NULL)
+		fatalError("Failed to create host thread");
+
+	// Run host
 	run(host);
+
+	// KTHXBAI
 	return 0;
 }
 
