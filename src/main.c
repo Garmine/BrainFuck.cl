38344dee3b28528c7ad0b593db7d3dfba019ec39
Garmine <mikro001@gmail.com>
2014-12-02 02:06:55 +0100

    Rewrote main.c for new host files

--- src/main.c
+++ src/main.c
@@ -1,13 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "host.h"
+
+// Prints "Schorbino!\n" to stdout
+const char* code = "\
+++++[>++++[>+++++<-]<-]>>+++.    83=S\
+<++++[>++++<-]>.                 99=c\
++++++.                          104=h\
++++++++.                        111=o\
++++.                            114=r\
+<++++[>----<-]>.                 98=b\
+>+++++[>++++++<-]>+++.<<         33=!\
+<+++[>++++<-]>.                 110=n\
++.                              111=o\
+>>>++[>+++++<-]>.                10=\n";
+
 int main(int argc, char** argv){
-	parse(programkod);
-
-	if (debug) printInstr();
-
-	run();
-
+	Host* host = initHost(code);
+	run(host);
 	return 0;
 }
 
