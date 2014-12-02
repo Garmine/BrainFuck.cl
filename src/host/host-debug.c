8ca46569432335b837714c175cefefd1ff72e355
Garmine <mikro001@gmail.com>
2014-12-03 00:26:20 +0100

    Redesigned instruction dump function for tapes of very large sizes.

--- src/host/host-debug.c
+++ src/host/host-debug.c
@@ -2,22 +2,22 @@
 
 #include "host-instructions.h"
 
+#include <stdio.h>
+
 #if DEBUG
 // prints the instruction and the parameters tape
 void printHostInstr(Host* host){
 	int pc;
-	printf("| ");
 	for(pc=0; pc<host->iLen; pc++){
+		printf("| ");
 		switch(geti(host->instr, pc)){
-			case DATA:	printf("DATA | ");	break;
-			case IO:	printf("  IO | ");	break;
-			case PTR:	printf(" PTR | ");	break;
-			case JMP:	printf(" JMP | ");	break;
+			case DATA:	printf("DATA");	break;
+			case IO:	printf("  IO");	break;
+			case PTR:	printf(" PTR");	break;
+			case JMP:	printf(" JMP");	break;
 		}
+		printf(" %4d |\n", host->param[pc]);
 	}
-	printf("\n| ");
-	for	(pc=0; pc<host->iLen; pc++) printf("%4d | ", host->param[pc]);
-	printf("\n");
 }
 #endif
 
--- src/host/host-debug.h
+++ src/host/host-debug.h
@@ -1,7 +1,9 @@
 #ifndef HOST_DEBUG_H
 #define HOST_DEBUG_H
 
-#define DEBUG 0
+#include "host.h"
+
+#define DEBUG 1
 
 #if DEBUG
 void printHostInstr(Host* host);
